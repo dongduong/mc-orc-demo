d89917296401618d7727507223750a6d4289c1f1
Dong Duong <dong.jobready@gmail.com>
2020-06-21 10:54:42 +0700

    update

--- app/controllers/accident_invoice_controller.rb
+++ app/controllers/accident_invoice_controller.rb
@@ -6,4 +6,24 @@ class AccidentInvoiceController < ApplicationController
   def new
     @invoice = AccidentInvoice.new
   end
+
+  def create
+    @invoice = AccidentInvoice.new(invoice_params)
+
+    if @invoice.save
+      redirect_to accident_invoice_path(@invoice), notice: 'Invoice is create Susscessful'
+    else
+      render :new, error: 'Failed to create new Invoice'
+    end
+  end
+
+  def show
+    @invoice = AccidentInvoice.find(params[:id])
+  end
+
+  private 
+
+  def invoice_params
+    params.require(:accident_invoice).permit(:invoice_file)
+  end
 end
